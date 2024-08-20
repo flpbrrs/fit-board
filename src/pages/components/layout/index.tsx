98356736291643b972c4fc6b2d5f59c589dff40f
Felipe Barros <felipejonathan.dev@gmail.com>
2024-08-20 19:32:59 -0300

    style: alteração dos espaçamentos do componente Page devido responsividade

--- src/pages/components/layout/index.tsx
+++ src/pages/components/layout/index.tsx
@@ -10,6 +10,7 @@ export default function Page(props: PageProps) {
     return (
         <main className={`
             flex flex-col gap-2 p-2 
+            sm:gap-3 sm:p-3
             lg:flex-row lg:gap-4 lg:p-4 lg:h-screen
         `}>
             <Header />
