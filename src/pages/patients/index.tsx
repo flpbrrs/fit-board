d0fcd1331bdbaa556411ed77ead78fcc1ab7e7e7
Felipe Barros <felipejonathan.dev@gmail.com>
2024-08-20 19:33:44 -0300

    style: estruturação do grid responsivo do dashboard de pacientes

---
+++ src/pages/patients/index.tsx
@@ -0,0 +1,41 @@
+import Page from "../components/layout";
+
+export default function Patient() {
+    return (
+        <Page>
+            <div className={`
+                grid grid-cols-1 gap-2
+                sm:grid-cols-2 sm:grid-rows-[reapeat(6, auto)] sm:gap-3
+                lg:grid-cols-12 lg:grid-rows-[repeat(4, auto)] lg:gap-4
+            `}>
+                <div className="card sm:col-span-2 lg:col-span-12">
+                    <div>Resumo user</div>
+                </div>
+                <div className="card lg:col-span-3">
+                    <div>Graf. Peso ao longo do tempo</div>
+                </div>
+                <div className="card lg:col-span-3">
+                    <div>Graf. Composição corporal</div>
+                </div>
+                <div className="card lg:col-span-3">
+                    <div>Graf. Macronutrientes</div>
+                </div>
+                <div className="card lg:col-span-3">
+                    <div>Graf. Consumo de água</div>
+                </div>
+                <div className="card sm:col-span-2 lg:col-span-4">
+                    <div>Objetivos e metas</div>
+                </div>
+                <div className="card lg:col-span-4">
+                    <div>Recomendações de atv. físicas</div>
+                </div>
+                <div className="card lg:col-span-4">
+                    <div>Próximas consultas</div>
+                </div>
+                <div className="card sm:col-span-2 lg:col-span-12">
+                    <div style={{ height: '800px'}}>Tabela dieta</div>
+                </div>
+            </div>
+        </Page>
+    )
+}
